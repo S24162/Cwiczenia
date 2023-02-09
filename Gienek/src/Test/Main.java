384fc4e1606bfe14125d379019b4342ca8ef5c23
Egrakovitch <dakarKTM690r@gmail.com>
2023-02-09 16:24:52 +0100

    First commit again

--- Gienek/src/Test/Main.java
+++ Gienek/src/Test/Main.java
@@ -1,17 +1,39 @@
 package Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
 
 //        List<Test> tests = new ArrayList<>();
 //        Func func= new Func( 0 , -4);
-        Test test = new Test(2,4,0 , -4);
+//        Test test = new Test(2,4,0 , -4);
 //        System.out.println(func);
-        System.out.println(test.y());
+//        System.out.println(test.y());
+        System.out.println("Fuck");
+
+//            int counter = 0;
+//            while (counter < 10)
+//            {
+//                System.out.println("Hello world!" + (counter+1));
+//                counter++;
+//            }
+//            System.out.println();
+        double a = 13000;
+        double b = 0.01;
+        double x = 0;
+            for (int i = 1; i <= 36; i++) {
+                x=x + a*0.01;
+                a = a - 361;
+            }
+        System.out.println(x);
+
+//            System.out.println();
+//            counter = 0;
+//            do {
+//                System.out.println("Hello world!" + (counter+1));
+//            counter++;
+//            }while (counter++ < 9);
 
     }
 }
+
